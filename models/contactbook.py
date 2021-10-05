2796a70672de5c5423d4b8a4cc43742f8026a92b
coderj001 <amirajubolchi001@gmail.com>
2021-10-05 23:08:35 +0530

    update find function

--- models/contactbook.py
+++ models/contactbook.py
@@ -27,7 +27,10 @@ class ContactBook(AuditMixin):
     async def update(cls, session: AsyncSession, **kwargs):
         if kwargs.get('id'):
             id = kwargs.pop('id')
-            ct_book = await session.execute(update(cls).where(cls.id == id).values(**kwargs).returning(cls))
+            ct_book = await session.execute(
+                update(cls).where(cls.id == id)
+                .values(**kwargs).returning(cls)
+            )
             return ct_book.fetchone()[0]
 
     @classmethod
@@ -37,8 +40,50 @@ class ContactBook(AuditMixin):
             page = int(kwargs.pop('page')) - 1
         else:
             page = 0
-        if (kwargs.get('name') and kwargs.get('email')) or (kwargs.get('name') or kwargs.get('email')):
-            ct_book = await session.execute(select(cls).where(**kwargs).offset(page*pagination).limit(10))
+        if (
+            kwargs.get('name') and kwargs.get('email')
+        ) or (
+            kwargs.get('name') or kwargs.get('email')
+        ):
+            if kwargs.get('name') and kwargs.get('email') is None:
+                ct_book = await session.execute(
+                    select(cls.id,
+                           cls.name,
+                           cls.email
+                           )
+                    .where(cls.name.like(f"{kwargs.get('name')}%"))
+                    .offset(page*pagination)
+                    .limit(10)
+                )
+            if kwargs.get('name') is None and kwargs.get('email'):
+                ct_book = await session.execute(
+                    select(cls.id,
+                           cls.name,
+                           cls.email
+                           )
+                    .where(cls.email.like(f"{kwargs.get('email')}%"))
+                    .offset(page*pagination)
+                    .limit(10)
+                )
+            else:
+                ct_book = await session.execute(
+                    select(cls.id,
+                           cls.name,
+                           cls.email
+                           ).where(
+                        cls.email.like(f"{kwargs.get('email')}%"),
+                        cls.name.like(f"{kwargs.get('name')}%")
+                    )
+                    .offset(page*pagination).limit(10)
+                )
         else:
-            ct_book = await session.execute(select(cls.id, cls.name, cls.email).offset(int(page)*pagination).limit(10))
+            ct_book = await session.execute(
+                select(
+                    cls.id,
+                    cls.name,
+                    cls.email
+                )
+                .offset(int(page)*pagination)
+                .limit(10)
+            )
         return ct_book.fetchmany()
